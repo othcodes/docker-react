f9def62a144f99cc20e55e396974fa87735f8e84
othcodes <bouesgaothmane@gmail.com>
2022-02-21 13:28:57 +0100

    travis deleted

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hi, I am a dummy text</p>
+        <p>Hi, I am a dummy text!!!!!</p>
         <a
           className='App-link'
           href='https://reactjs.org'
