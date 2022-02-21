bfea406ec62aefedd90b64a3e1e94a710a275be9
othcodes <bouesgaothmane@gmail.com>
2022-02-21 15:28:18 +0100

    fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-<<<<<<< HEAD
         <p>Hello world, it's me!</p>
-=======
-        <p>Hi, I am a dummy text!!!!!</p>
->>>>>>> 897f150c6d1c50214d87fa66bc64193294627720
         <a
           className='App-link'
           href='https://reactjs.org'
