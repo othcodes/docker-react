b26afeeb945377f0c123def3fd8be606cb09a632
othcodes <bouesgaothmane@gmail.com>
2022-02-21 13:12:24 +0100

    app text updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello I am Othmane</p>
+        <p>Hi, I am a dummy text</p>
         <a
           className='App-link'
           href='https://reactjs.org'
